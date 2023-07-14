948e14b1eac434f31c4a44be95622ac1d910a642
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-07-14 13:50:10 -0400

    ranged enemy movement

--- Assets/Scripts/Gameplay/EnemyControllers/RangedEnemyController.cs
+++ Assets/Scripts/Gameplay/EnemyControllers/RangedEnemyController.cs
@@ -9,13 +9,17 @@ public class RangedEnemyController : EnemyController
     public float SpreadAngle = 3f;
 
     [Header("Ranged Enemy - Movement")]
+    public float GoalSwapPeriod = 5f;
     public float MinGoalRange = 5f;
     public float MaxGoalRange = 9f;
     public float MinSpeed = 1.5f;
     public float MaxSpeed = 2f;
+    
+    public float GoalToleranceThreshold = 0.5f;
 
     // private variables
     private Vector2 _targetVelocity = Vector2.zero;
+    private float _goalSwapTimer;
     private float _goalRange;
     private float _goalSpeed;
     private bool _clockwise;
@@ -24,6 +28,11 @@ public class RangedEnemyController : EnemyController
     protected override void Start()
     {
         base.Start();
+
+        _goalSwapTimer = GoalSwapPeriod;
+        _goalRange = Random.Range(MinGoalRange, MaxGoalRange);
+        _goalSpeed = Random.Range(MinSpeed, MaxSpeed);
+        _clockwise = Random.Range(0f, 1f) > 0.5f ? true : false;
     }
 
     // Update is called once per frame
@@ -41,6 +50,25 @@ public class RangedEnemyController : EnemyController
 
     protected override void UpdateMovement()
     {
+        // conduct goal swap every period
+        _goalSwapTimer -= Time.deltaTime;
+        if(_goalSwapTimer < 0f)
+        {
+            _goalSwapTimer = GoalSwapPeriod;
+            _goalRange = Random.Range(MinGoalRange, MaxGoalRange);
+            _goalSpeed = Random.Range(MinSpeed, MaxSpeed);
+            _clockwise = Random.Range(0f, 1f) > 0.5f ? true : false;
+        }
+
+        float distance = Vector2.Distance(_player.transform.position, transform.position);
+        Vector2 playerDirection = ((Vector2)_player.transform.position - (Vector2)transform.position).normalized;
+        Vector2 clockwiseDirection = -1 * Vector2.Perpendicular(playerDirection);
+
+        // generate target velocity
+        // _targetVelocity = ((distance > _goalRange ? 1 : -1) * playerDirection + (_clockwise ? 1 : -1) * clockwiseDirection).normalized * _goalSpeed;
+        _targetVelocity = Mathf.Abs(distance - _goalRange) > GoalToleranceThreshold ? 
+            (distance > _goalRange ? 1 : -1) * playerDirection * _goalSpeed : (_clockwise ? 1 : -1) * clockwiseDirection * _goalSpeed;
+
         // update velocity based on target and current velocities
         Rb.velocity = Vector2.Lerp(Rb.velocity, _targetVelocity, 1 - Mathf.Exp(-MovementSharpness * Time.deltaTime));
     }

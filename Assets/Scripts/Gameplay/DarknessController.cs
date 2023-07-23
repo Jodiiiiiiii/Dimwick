c63b17fe3ed8748f75fc628cb33c3e484e29ccd0
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-07-23 13:02:41 -0400

    darkness boss movement and dash attack

--- Assets/Scripts/Gameplay/DarknessController.cs
+++ Assets/Scripts/Gameplay/DarknessController.cs
@@ -7,6 +7,13 @@ public class DarknessController : MonoBehaviour
     [Header("HP")]
     public float MaxHP = 5f;
 
+    [Header("Movement")]
+    public float GoalSwapPeriod = 5f;
+    public float BaseMoveSpeed = 2f;
+    public float DashDuration = 1.5f;
+    public float DashMoveSpeed = 8f;
+    public float MovementSharpness = 10f;
+
     [Header("Attacking")]
     public float MinAttackCooldown = 3f;
     public float MaxAttackCooldown = 8f;
@@ -15,15 +22,20 @@ public class DarknessController : MonoBehaviour
     public GameObject SpawnProjectile;
 
     [HideInInspector] public Animator Anim;
+    [HideInInspector] public Rigidbody2D Rb;
 
+    private Vector2 _targetVelocity = Vector2.zero;
+    private float _goalSwapTimer;
     private float _attackCooldownTimer;
-    private bool _isDashing = false;
+    private bool _dashReady = false;
 
     // Start is called before the first frame update
     void Start()
     {
         Anim = GetComponent<Animator>();
+        Rb = GetComponent<Rigidbody2D>();
 
+        _goalSwapTimer = GoalSwapPeriod;
         _attackCooldownTimer = Random.Range(MinAttackCooldown, MaxAttackCooldown);
     }
 
@@ -31,6 +43,27 @@ public class DarknessController : MonoBehaviour
     void Update()
     {
         #region MOVEMENT
+        if(_goalSwapTimer <= 0) // set new target
+        {
+            float randAngle = Random.Range(0f, 360f);
+            if (_dashReady)
+            {
+                _targetVelocity = new Vector2(Mathf.Cos(randAngle), Mathf.Sin(randAngle)) * DashMoveSpeed;
+                _goalSwapTimer = DashDuration;
+                _dashReady = false;
+            }
+            else // default movemenet
+            {
+                _targetVelocity = new Vector2(Mathf.Cos(randAngle), Mathf.Sin(randAngle)) * BaseMoveSpeed;
+                _goalSwapTimer = GoalSwapPeriod;
+            }
+        }
+        else
+        {
+            _goalSwapTimer -= Time.deltaTime;
+        }
+        Debug.Log(_targetVelocity);
+        Rb.velocity = Vector2.Lerp(Rb.velocity, _targetVelocity, 1 - Mathf.Exp(-MovementSharpness * Time.deltaTime));
         #endregion
 
         #region ATTACKING
@@ -48,7 +81,7 @@ public class DarknessController : MonoBehaviour
             }
             else // dash attack
             {
-                _isDashing = true;
+                _dashReady = true;
             }
 
             // restart attack cooldown

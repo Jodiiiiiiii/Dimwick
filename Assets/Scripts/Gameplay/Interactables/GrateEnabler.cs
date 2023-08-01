f5f15e07a2493a9de4df6c2ecce51798a902625d
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-08-01 15:34:17 -0400

    grate opening sound

--- Assets/Scripts/Gameplay/Interactables/GrateEnabler.cs
+++ Assets/Scripts/Gameplay/Interactables/GrateEnabler.cs
@@ -6,9 +6,15 @@ public class GrateEnabler : MonoBehaviour
 {
     [Tooltip("object containing all enemies in the scene as children")]
     public GameObject EnemyContainer;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip OpenClip;
+    [Range(0f,1f)] [SerializeField] private float OpenVolume;
     
     [HideInInspector] public GrateTransition TransitionInteractable;
     [HideInInspector] public Animator Anim;
+
+    private bool _isOpened = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +26,14 @@ public class GrateEnabler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(EnemyContainer.GetComponentsInChildren<Transform>().Length == 1) // only parent empty remains
+        if(EnemyContainer.GetComponentsInChildren<Transform>().Length == 1 && !_isOpened) // only parent empty remains
         {
             Anim.enabled = true;
             TransitionInteractable.enabled = true;
+
+            GameManager.instance.PlaySound(OpenClip, OpenVolume);
+
+            _isOpened = true; // prevent looping sound
         }
     }
 }

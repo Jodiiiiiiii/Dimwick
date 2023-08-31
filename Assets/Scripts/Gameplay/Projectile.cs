837f237c2b949301aef9b8cfd24062268284d5be
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-08-31 13:48:16 -0400

    playtesting feedback/balancing

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -102,7 +102,6 @@ public class Projectile : MonoBehaviour
 
         // walls destroy enemy projectiles, enemy projectiles destroy player projectiles
         if ((DestroyOnCollision && collision.CompareTag("Wall"))
-            || (gameObject.CompareTag("PlayerBullet") && (collision.CompareTag("EnemyBullet") || collision.CompareTag("SpawnProjectile")))
             || (gameObject.CompareTag("EnemyBullet") && collision.CompareTag("LightBlast")))
             Destroy(gameObject);
     }
